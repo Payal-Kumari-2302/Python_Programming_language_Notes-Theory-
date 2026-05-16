517c8942d781f9168ec0861378d2d0dad05eae45
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-16 09:54:51 +0530

    Add introduction to Python programming language
    This repository contains Python programming notes including introduction, history, features, versions, limitation, applications, and basic concepts in a simple and beginner-friendly format.

---
+++ introduction-to-python.py
@@ -0,0 +1,95 @@
+🐍 Programming in Python:-
+
+
+📌 Introduction to Python Language:-
+
+👉 Python is a general-purpose, dynamic, high-level, and interpreted programming language.
+
+👉 It was created in 1991 with a focus on code readability and fewer lines of code.
+
+👉 Python is simple and easy to learn, making it ideal for beginners.
+
+👉 It supports Object-Oriented Programming (OOP) for building organized applications.
+
+
+
+📌 History of Python Language:-
+
+👉 Python was created by Guido van Rossum in 1991 at CWI (Netherlands).
+
+👉 It was designed as a successor to the ABC language.
+
+👉 The name “Python” came from the comedy show Monty Python’s Flying Circus.
+
+
+
+🐍 Python Versions:-
+
+👉 Python 1.x (1990s): Basic features
+
+👉 Python 2.x (2000): Improved performance and garbage collection
+
+👉 Python 3.x (2008 – Present): Better syntax, Unicode support, and security
+
+
+
+🐍 Features of Python:-
+
+👉 Simple and readable syntax
+👉 Dynamic typing
+👉 Large standard library
+👉 Object-Oriented Programming
+👉 Cross-platform support
+
+
+
+❌ Limitations of Python:-
+
+👉 Slow execution speed
+👉 High memory usage
+👉 Not ideal for mobile apps
+👉 Weak in high-performance games
+👉 Limited multithreading (GIL issue)
+
+
+
+🚀 Applications of Python:-
+
+👉 Web Development (Django, Flask)
+👉 Data Science
+👉 AI & Machine Learning
+👉 Automation
+👉 Software Development
+👉 Cyber Security
+
+
+
+🤔 Why Learn Python:-
+
+👉 Easy to learn and read
+👉 High demand in jobs
+👉 Large community support
+👉 Used in many fields like AI, web, data science
+
+
+
+🧮 Syntax & Example:-
+
+Syntax:-
+print("write a Message")
+
+📖 Explanation:
+
+👉 print() → Built-in function used to show output
+
+👉 ( Opening parenthesis → Starts the input (argument) for the function
+
+👉 "message" → String (text) that must be written in single (' ') or double (" ") quotes
+
+👉 ) Closing parenthesis → Ends the function input
+
+
+example:-
+print("Hello World!")
+
+
