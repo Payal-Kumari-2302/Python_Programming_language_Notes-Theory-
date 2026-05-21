73ad07f6302b2a297589053523f30441647b09a6
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-21 11:14:45 +0530

    Document Python input and output functionality
    Python Input and Output functions are used to take data from the user and display results on the screen.

---
+++ 2nd_Topic_Python_input_output.py
@@ -0,0 +1,58 @@
+🐍 Python Input & Output:-
+
+📥 Python Input:- Python input is used to take data from the user while the program is running.
+
+🔹 input() Function:- Python uses the input() function to get input from the user.
+
+📝 Note:- input() function always takes input as a string.
+
+
+💻 Example:
+ 
+name = input("Enter Your Name:")
+print("Your Name is:", name)
+
+🔢 Taking Number Input
+
+Since input() takes data as a string, we convert it into numbers.
+
+int() ➝ Integer number
+float() ➝ Decimal number
+
+
+💻 Example:
+age = int(input("🎂 Enter Your Age: "))
+
+salary = float(input("💰 Enter Salary: "))
+
+print("Age =", age)
+print("Salary =", salary)
+
+
+
+
+
+🖥️ Python Output:- Output in Python means displaying data or results on the screen.
+
+🔹 print() Function:- Python uses the print() function to show the output.
+
+💻 Example_1:-
+
+print("Hello World")
+
+💻 Example_2:-
+
+x = 10
+print(x)
+
+
+💻 Example_3:
+
+name = "Payal"
+age = 20
+
+print("Name:", name)
+print("Age:", age)
+
+
+
