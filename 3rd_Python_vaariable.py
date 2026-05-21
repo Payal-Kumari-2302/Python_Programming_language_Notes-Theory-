6853b011a5534cdf8fb58804fbf722349a933f32
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-21 11:25:18 +0530

    Document Python variable types and usage
    Added explanations and examples of Python variables, including local and global variables.

---
+++ 3rd_Python_vaariable.py
@@ -0,0 +1,48 @@
+🐍 Python Variable:-
+
+🔹 A variable is a name given to a memory location
+🔹 It is used to store data in memory
+🔹 It helps to retrieve data whenever needed
+🔹 Variables can store different types of data (int, string, float)
+
+🔹 Example:-
+x = 10
+name = "Payal"
+price = 99.5
+
+
+🐍 Types of Variables in Python:-
+
+1.Local Variable
+2.Globle Variable
+
+
+1. Local Variable
+🔹 A variable declared inside a function
+🔹Used only within that function
+🔹Cannot be accessed outside the function
+
+💻 Example:-
+
+def demo():
+    x = 10       # Local variable
+    print(x)
+demo()
+
+2. Global Variable
+🔹A variable declared outside all functions
+🔹Can be used anywhere in the program
+🔹Accessible inside and outside functions
+
+💻 Example:
+
+x = 20         # Global variable
+def demo():
+    print(x)
+demo()
+print(x)
+
+
+
+
+
